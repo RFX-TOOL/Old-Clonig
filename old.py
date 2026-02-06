502e1cba0ca774f506ff5dd140815bb43d21e613
RFX-TOOL <razalammim@gmail.com>
2026-02-06 10:52:21 +0600

    Update old.py

--- old.py
+++ old.py
@@ -17,7 +17,7 @@ os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
 if bit == '64bit':
     print('\033[1;97m[✓] \033[1;92mFound 64 Bit Device')
-    import old
+    import old_up
 elif bit == '32bit':
     print('\033[1;97m[✓] \033[1;91mSORRY BRO YOUR DEVICE IS 32 BIT')
     print('\033[1;97m[✓] \033[1;91mPLEASE USE 64 BIT DEVICE')
