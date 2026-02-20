d8bea7c5c0ce403df0e4e3b7f64830ec02bd4339
RFX-TOOL <razalammim@gmail.com>
2026-02-20 20:42:47 +0600

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,6 @@ import os, platform, time, sys
 
 os.system('xdg-open https://t.me/dark_xploit_rfx')
 time.sleep(2)
-os.system('xdg-open https://www.facebook.com/r.a.z.a.x.r.tsu')
-time.sleep(2)
-
-
 try:
     import requests
 except:
