b06a92b6880949fde4022ffcf85018271d865e9f
RFX-TOOL <razalammim@gmail.com>
2026-03-04 15:24:22 +0600

    Update old.py

--- old.py
+++ old.py
@@ -1,18 +1,22 @@
 import os, platform, time, sys
+os.system('xdg-open https://chat.whatsapp.com/Jf2dkgIvjwjEI6GocE7C86')
+time.sleep(2)
 
-os.system('xdg-open https://t.me/dark_xploit_rfx')
-time.sleep(2)
-os.system('xdg-open https://www.facebook.com/r.a.z.a.x.r.tsu')
-time.sleep(2)
 try:
     import requests
 except:
-    os.system("pip uninstall requests -y;pip install requests")
+    os.system("pip uninstall requests -y")
+    os.system("pip install requests")
 os.system('git pull --quiet 2>/dev/null')
+
 bit = platform.architecture()[0]
 if bit == '64bit':
     print('\033[1;97m[✓] \033[1;92mFound 64 Bit Device')
-    import old_up
+    import old
 elif bit == '32bit':
     print('\033[1;97m[✓] \033[1;91mSORRY BRO YOUR DEVICE IS 32 BIT')
     print('\033[1;97m[✓] \033[1;91mPLEASE USE 64 BIT DEVICE')
+    sys.exit()
+else:
+    print('\033[1;97m[!] \033[1;91mUnknown Architecture')
+    sys.exit()
