c573e262d3c535254eea9eb1ff65b4ec5d8c0218
RFX-TOOL <razalammim@gmail.com>
2026-06-02 13:39:26 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
 import os, platform, time, sys
-os.system('xdg-open https://chat.whatsapp.com/DFj8atKbJZYE1zc68YMnvK?mode=gi_t')
-time.sleep(2)
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import old
-elif bit == '32bit':
-    print('\033[1;97m[✓] \033[1;91mSORRY BRO YOUR DEVICE IS 32 BIT')
-    print('\033[1;97m[✓] \033[1;91mPLEASE USE 64 BIT DEVICE')
-    sys.exit()
-else:
-    print('\033[1;97m[!] \033[1;91mUnknown Architecture')
-    sys.exit()
+
+colors = ['\033[1;97m', '\033[1;92m', '\033[1;93m']
+
+while True:
+    for color in colors:
+        print(f"{color}CURRENTLY THIS TOOL IS OFFLINE\n")
+        time.sleep(1)
