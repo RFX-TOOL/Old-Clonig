87f81bc8e3fd0eb980d777a0e345b9413ddacd3f
RFX-TOOL <razalammim@gmail.com>
2026-06-09 09:43:02 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,1 @@
-import os, platform, time, sys
-
-colors = ['\033[1;97m', '\033[1;92m', '\033[1;93m']
-
-while True:
-    for color in colors:
-        print(f"{color}CURRENTLY THIS TOOL IS OFFLINE\n")
-        time.sleep(1)
+import old
